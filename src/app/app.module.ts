7db8fe589413a380d12f8b13c2a40209d5b1e284
robertoruelas <ingrobertoruelas@gmail.com>
2019-09-18 18:27:16 -0600

    primerCommt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './pages/page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component
   ],
   imports: [
     BrowserModule
